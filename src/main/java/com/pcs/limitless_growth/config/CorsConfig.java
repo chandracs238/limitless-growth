089da663f9d8ea049767d9b6caaa30ff39e66f01
chandracs238 <chandhu7013@gmail.com>
2025-03-08 14:22:52 +0530

    Pushing Project

---
+++ src/main/java/com/pcs/limitless_growth/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.pcs.limitless_growth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // Apply to all endpoints
+                        .allowedOrigins("http://localhost:3000") // Allowed origins
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // Allowed methods
+                        .allowedHeaders("*"); // Allow all headers
+            }
+        };
+    }
+}
